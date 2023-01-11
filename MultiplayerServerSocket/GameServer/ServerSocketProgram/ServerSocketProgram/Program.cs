130cc9fd703f8fd34fe59e894d74ca1ace8efec2
Rudra <122004106+Coldblot42@users.noreply.github.com>
2023-01-11 11:48:17 +0530

    GameServer

---
+++ MultiplayerServerSocket/GameServer/ServerSocketProgram/ServerSocketProgram/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ServerSocketProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Server to connect");
+            Server.Start(5, 11000); 
+
+            Console.ReadLine();
+        }
+    }
+}
